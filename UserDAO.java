d5e8e3581628484a140d22875a98ad3729b920f9
Saumya raj <saumyaraj528@gmail.com>
2024-11-10 22:14:30 +0530

    Add files via upload

---
+++ UserDAO.java
@@ -0,0 +1,10 @@
+package com.user.dao;
+
+public class UserDAO {
+	
+	private String jdbcURL="jdbc:mysql://localhost:30006/quiz_db";
+	private String jdbcUserName="root";
+	private String jdbcPassword="root";
+	
+
+}
