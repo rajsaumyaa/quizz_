87d98aae5b7d2dce8f32e530c898eefb1321e6a1
Saumya raj <saumyaraj528@gmail.com>
2024-11-14 08:36:24 +0530

    Update UserDAO.java

--- UserDAO.java
+++ UserDAO.java
@@ -8,3 +8,40 @@ public class UserDAO {
 	
 
 }
+ public boolean registerUser (User user) {
+        String sql = "INSERT INTO Users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPassword()); 
+            pstmt.setString(3, user.getEmail());
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public User loginUser (String username, String password) {
+        String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password); 
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new User(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null; 
+    }
+}
